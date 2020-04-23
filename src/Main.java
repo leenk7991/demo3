a3f78aab7b521fcf945ee4d80c21511fe25e6abe
Leen <leen.kilani@gmail.com>
2020-04-23 13:26:14 -0400

    added new feature: good morning

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("good afternoon");
         System.out.println("Good morning");
+        System.out.println("new feature");
     }
 }
